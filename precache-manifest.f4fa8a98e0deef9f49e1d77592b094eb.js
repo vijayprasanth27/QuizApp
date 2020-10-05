0f720834d707ff5aa86a685b181ae18d953bb54e
vijay prasanth <vijayprasanth27@gmail.com>
2020-10-05 18:21:06 +0530

    Updates

---
+++ precache-manifest.f4fa8a98e0deef9f49e1d77592b094eb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60271aefee34b8ef6d9051d842cd373d",
+    "url": "/QuizApp/index.html"
+  },
+  {
+    "revision": "875758e9abfe611b022b",
+    "url": "/QuizApp/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "582b0062931f6026a8ac",
+    "url": "/QuizApp/static/css/main.c87d242a.chunk.css"
+  },
+  {
+    "revision": "875758e9abfe611b022b",
+    "url": "/QuizApp/static/js/2.c2e992a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/QuizApp/static/js/2.c2e992a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "582b0062931f6026a8ac",
+    "url": "/QuizApp/static/js/main.74d80e32.chunk.js"
+  },
+  {
+    "revision": "0bd87255aeab8a742865",
+    "url": "/QuizApp/static/js/runtime-main.fe33ff4d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/QuizApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
